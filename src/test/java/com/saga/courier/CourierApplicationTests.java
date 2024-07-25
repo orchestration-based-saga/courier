f925deb54f44e86660c077169ade7ce42f5deb3f
marijal74 <lakic.marija98@gmail.com>
2024-07-25 21:33:16 +0200

    Init repo

---
+++ src/test/java/com/saga/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
